0aec8585f9a5f1b895e776b4ec6df4cdb82437fb
Jeff Cogswell <jeffcogs@gmail.com>
2022-09-01 11:36:29 -0400

    Added Cash Register

---
+++ Register/CashRegister.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Register
+{
+	public class CashRegister
+	{
+		// One function: Takes a list of products
+		// and calculates the grand total price.
+
+		public static decimal GrandTotal(List<Product> prods)
+		{
+			decimal total = 0.0m;
+			foreach (Product prod in prods)
+			{
+				total += prod.Price;
+			}
+			return total;
+		}
+
+		// Second function: Takes a list of products
+		// and calculates the sales tax. (Say 6% or
+		// whatever you like.)
+	}
+}
---
+++ Register_Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using Register;
+
+namespace Register_Tests
+{
+	public class UnitTest1
+	{
+		// Don't try to use [Theory]
+		// Just make individual tests with [Fact]
+		// Inside each test, create a list of 
+		// products, and call the function
+		// and compare actual to expected.
+
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
